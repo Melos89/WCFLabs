51f11e2395f42f2fe55959ffe28b2bb92a27cf94
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-18 14:34:53 +0100

    Started Laboration 2, adding services to the Laboration 1 solution, creating a seperate solution for clients later.

---
+++ wcfkurs.se.ASMXLABB1/BMIService/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace BMIService
+{
+    [ServiceContract(Namespace ="http://BMIService")]
+    public interface IBMICounter
+    {
+        [OperationContract]
+        float CalculateBMI(float weight,float height);
+    }
+    public class BMICounterService : IBMICounter
+    {
+        public float CalculateBMI(float weightinKilos, float heightinCentimetres)
+        {
+            return (weightinKilos / heightinCentimetres) * weightinKilos;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAdress = new Uri("http://localhost:8080/BMIService");
+
+            using (var host = new ServiceHost(typeof(BMICounterService),baseAdress))
+            {
+                host.AddServiceEndpoint(typeof(IBMICounter),new WSHttpBinding(),"BMICounterService");
+
+                var smB = new ServiceMetadataBehavior();
+                smB.HttpGetEnabled = true;
+
+                host.Description.Behaviors.Add(smB);
+
+                host.Open();
+                Console.WriteLine("Service is open for business!");
+                Console.WriteLine("Press Enter to kill the service");
+                Console.ReadKey();
+            }
+        }
+    }
+}
