cabb6418975ebfc3b0317262d3e6bc6b141de2bb
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-19 14:13:08 +0100

    Finished part 3/4 service, Date When next turning an even thousand days

---
+++ wcfkurs.se.ASMXLABB1/DaysUntilNextHosting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using DaysUntilNextThousandDays;
+
+namespace DaysUntilNextHosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAdress = new Uri("http://localhost:8080/NextThousandDayService");
+
+            using (var host = new ServiceHost(typeof(NextThousandDaysService),baseAdress))
+            {
+                host.AddServiceEndpoint(typeof(INextThousandDays),new WSHttpBinding(),"NextThousandDaysService");
+
+                var smB = new ServiceMetadataBehavior();
+                smB.HttpGetEnabled = true;
+
+                host.Description.Behaviors.Add(smB);
+
+                host.Open();
+
+                Console.WriteLine("Service is now open!");
+                Console.WriteLine("To close it press Enter");
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
