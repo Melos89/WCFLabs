5b8bfa72fdd0314470f1e559b1cde2e9a0e9e5c1
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-19 15:13:55 +0100

    Finished with clients for Part 3/4 Thousand days.

--- ClientsLABB2/ThousandDaysClientConsole/Program.cs
+++ ClientsLABB2/ThousandDaysClientConsole/Program.cs
@@ -10,12 +10,17 @@ namespace ThousandDaysClientConsole
     {
         static void Main(string[] args)
         {
-            var proxy
+            var proxy = new NextThousandDaysClient();
+
             Console.WriteLine("Provide your date of birth in a yyyy/mm/dd format:");
 
             var date = Console.ReadLine();
 
+            var result = proxy.DaysToGo(DateTime.Parse(date));
 
+
+            Console.WriteLine($"next time you have an even thousand days to your age is: {result.ToString()}");
+            Console.ReadKey();
         }
     }
 }
---
+++ ClientsLABB2/ThousandDaysClientWinForms/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThousandDaysClientWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DateButton_Click(object sender, EventArgs e)
+        {
+            var proxy = new NextThousandDaysClient();
+
+            var date = dateBox.Text;
+
+            ResultLabel.Text = proxy.DaysToGo(DateTime.Parse(date)).ToString();
+        }
+    }
+}
