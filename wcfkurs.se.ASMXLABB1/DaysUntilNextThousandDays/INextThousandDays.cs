a4ae2f9a963cfb5081bdc0410e474a93969c1478
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-19 12:17:54 +0100

    Added Part 3/4 Services, In labb 2

---
+++ wcfkurs.se.ASMXLABB1/DaysUntilNextThousandDays/INextThousandDays.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace DaysUntilNextThousandDays
+{
+    [ServiceContract(Namespace = "DaysUntilNextThousandDays")]
+    public interface INextThousandDays
+    {
+        [OperationContract]
+        int DaysToGo(DateTime date);
+    }
+}
---
+++ wcfkurs.se.ASMXLABB1/DaysUntilNextThousandDays/NextThousandDaysService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysUntilNextThousandDays
+{
+    public class NextThousandDaysService : INextThousandDays
+    {
+        public int DaysToGo(DateTime date)
+        {
+            var today = DateTime.Now;
+            var resultTime = (today - date).TotalDays;
+
+
+        }
+    }
+}
