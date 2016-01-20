a95b5be61544adaa087fefc6be4f03a6d909003c
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-20 10:42:13 +0100

    Started on 4/4 of labb 4, Service related to hobby, Character Generator Service

---
+++ wcfkurs.se.ASMXLABB1/CharacterGeneratorService/CharacterCreatorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterGeneratorService
+{
+    class CharacterCreatorService : ICharacterCreator
+    {
+        public Character GenerateCharacter(string name, string classType, int age)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ wcfkurs.se.ASMXLABB1/CharacterGeneratorService/ICharacterCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace CharacterGeneratorService
+{
+    [ServiceContract(Namespace ="http://CharacterGeneratorService")]
+    public interface ICharacterCreator
+    {
+        [OperationContract]
+        Character GenerateCharacter(string name, string classType, int age);
+    }
+}
